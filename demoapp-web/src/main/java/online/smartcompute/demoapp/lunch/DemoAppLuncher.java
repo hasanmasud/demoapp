65d1e361f8a8e0d6b1c3659e35a51ae7d0292c2b
Masud Hasan <masudbook@gmail.com>
2017-11-06 01:44:52 +0100

    updated

--- demoapp-web/src/main/java/online/smartcompute/demoapp/lunch/DemoAppLuncher.java
+++ demoapp-web/src/main/java/online/smartcompute/demoapp/lunch/DemoAppLuncher.java
@@ -1,27 +1,35 @@
 package online.smartcompute.demoapp.lunch;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 import org.apache.catalina.startup.Tomcat;
 
+/**
+ * 
+ * @author masud
+ *
+ */
 public class DemoAppLuncher {
 
 	public static final String DEFAULT_PORT = "8086";
 	public static final String CONTEXT_ROOT = "demoapp";
+	public static final Logger LOGGER = Logger.getLogger("DemoAppLuncher");
 
+	/**
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
-
 		Tomcat tomcat = new Tomcat();
-
 		// The port that we should run on can be set into an environment variable
-		// Look for that variable and default to 8080 if it isn't there.
+		// Look for that variable and default to 8086 if it isn't there.
 		String webPort = System.getenv("PORT");
 		if (webPort == null || webPort.isEmpty()) {
 			webPort = DEFAULT_PORT;
 		}
-
 		tomcat.setPort(Integer.valueOf(webPort));
-
 		// This is to compensate for the different paths that the appassembler-program
 		// and jsw daemon use
 		String basedir = System.getProperty("basedir");
@@ -33,12 +41,10 @@ public class DemoAppLuncher {
 		} else {
 			basedir = "src/main/webapp";
 		}
-
-		System.out.println("Basedir: " + basedir);
+		LOGGER.info("Basedir: " + basedir);
 		String webappLocation = new File(basedir).getAbsolutePath();
-		System.out.println("configuring app with basedir: " + webappLocation);
+		LOGGER.info("configuring app with basedir: " + webappLocation);
 		tomcat.addWebapp("/" + CONTEXT_ROOT, webappLocation);
-
 		tomcat.start();
 		tomcat.getServer().await();
 	}
